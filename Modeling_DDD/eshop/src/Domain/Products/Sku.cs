49f213482edd59edd861cc9062fa078a89ed26de
CalebTek <olawale.odeyemi@decagon.dev>
2024-04-26 21:38:29 +0100

    Created Product entity

---
+++ Modeling_DDD/eshop/src/Domain/Products/Sku.cs
@@ -0,0 +1,18 @@
+﻿namespace Domain.Products
+{
+    // Stock Keeping Unit
+    public record Sku
+    {
+        private const int DefaultLength = 15;
+        public string Value { get; private set; }
+        private Sku(string value) => Value = value;
+
+
+        public static Sku? Create(string value)
+        {
+            if (string.IsNullOrEmpty(value)) return null;
+            if (value.Length != DefaultLength) return null;
+            return new Sku(value);
+        }
+    }
+}
