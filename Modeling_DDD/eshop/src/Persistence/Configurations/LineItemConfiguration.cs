4367709fbb6481b32567fe64422c2e6e26371fa9
CalebTek <olawale.odeyemi@decagon.dev>
2024-04-26 23:46:40 +0100

    Create LineItemConfiguration.cs

---
+++ Modeling_DDD/eshop/src/Persistence/Configurations/LineItemConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Domain.Orders;
+using Domain.Products;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Persistence.Configurations
+{
+    internal class LineItemConfiguration : IEntityTypeConfiguration<LIneItem>
+    {
+        public void Configure(EntityTypeBuilder<LIneItem> builder)
+        {
+           builder.HasKey(li => li.Id);
+
+            builder.Property(li => li.Id).HasConversion(
+                lineItemId => lineItemId.Value, 
+                value => new LIneItemId(value));
+
+            builder.HasOne<Product>()
+                .WithMany().HasForeignKey(li => li.ProductId);
+
+            builder.OwnsOne(li => li.Price, priceBuilder =>
+            {
+                priceBuilder.Property(m => m.Currency).HasMaxLength(3);
+            });
+        }
+    }
+}
