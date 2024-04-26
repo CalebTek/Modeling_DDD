d996317fd4f66a6459e7bdd298c2c547d78eed6b
CalebTek <olawale.odeyemi@decagon.dev>
2024-04-26 22:52:35 +0100

    Created application context

---
+++ Modeling_DDD/eshop/src/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class ApplicationDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.UseNpgsql();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
+
+    }
+}
