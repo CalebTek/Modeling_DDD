467f67620912d2447dadd13669b439c5202d4bbb
CalebTek <olawale.odeyemi@decagon.dev>
2024-04-27 19:00:51 +0100

    Create RemoveLineItemCommand.cs

---
+++ Modeling_DDD/eshop/src/Application/Orders/RemoveLineItem/RemoveLineItemCommand.cs
@@ -0,0 +1,7 @@
+﻿using Domain.Orders;
+using MediatR;
+
+namespace Application.Orders.RemoveLineItem
+{
+    public record RemoveLineItemCommand(OrderId OrderId, LineItemId LineItemId) : IRequest;
+}
