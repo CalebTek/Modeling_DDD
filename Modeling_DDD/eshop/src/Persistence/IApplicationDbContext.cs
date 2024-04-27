982c331bbdc737e48d4393dcad42b92daecde0af
CalebTek <olawale.odeyemi@decagon.dev>
2024-04-28 00:13:29 +0100

    Create IApplicationDbContext.cs

---
+++ Modeling_DDD/eshop/src/Persistence/IApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Customers;
+using Domain.Orders;
+using Domain.Products;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
+namespace Persistence
+{
+    internal interface IApplicationDbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<LineItem> LineItems { get; set; }
+
+        public DbSet<OrderSummary> OrderSummaries { get; set; }
+
+        DatabaseFacade Database {  get; set; }
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+    }
+}
