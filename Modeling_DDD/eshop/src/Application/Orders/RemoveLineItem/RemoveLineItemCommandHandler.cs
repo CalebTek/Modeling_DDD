fbb88a7b8a166d1a8e25e42aa1612e7c9e20c34a
CalebTek <olawale.odeyemi@decagon.dev>
2024-04-27 19:03:29 +0100

    Create RemoveLineItemCommandHandler.cs

---
+++ Modeling_DDD/eshop/src/Application/Orders/RemoveLineItem/RemoveLineItemCommandHandler.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using Persistence;
+
+namespace Application.Orders.RemoveLineItem
+{
+   
+    public class RemoveLineItemCommandHandler : IRequestHandler<RemoveLineItemCommand>
+    {
+        private readonly ApplicationDbContext _context;
+        public Task Handle(RemoveLineItemCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
